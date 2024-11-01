29c2d9ed4188d72f39f363dcd67a7be7c6f3383b
abdeldayem8 <mhmdabdeldayem40@gmail.com>
2024-11-01 18:40:18 +0200

    add slick slider

--- src/components/ecommerce/ProductList/ProductList.jsx
+++ src/components/ecommerce/ProductList/ProductList.jsx
@@ -3,7 +3,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import useFetch from '../../../hooks/useFetch';
-import './productlist.css'
+
 
 
 const ProductList = () => {
