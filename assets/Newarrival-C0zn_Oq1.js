0e1f969e68498e3349cc778cc41d3655e932ba67
abdeldayem8 <abdeldayem8@users.noreply.github.com>
2024-11-25 17:46:06 +0000

    deploy: 5e9e69564fd0caffdfb1118075804897f9887622

---
+++ assets/Newarrival-C0zn_Oq1.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CzMRR9IY.js";const s=()=>r.jsx("div",{children:"New Arrivals"});export{s as default};
--- assets/Newarrival-DT6Ctwjx.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DkM6r4op.js";const s=()=>r.jsx("div",{children:"New Arrivals"});export{s as default};
