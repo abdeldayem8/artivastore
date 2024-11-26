7ffbbdc33cac32153448594ab5f8eac5bc707f5a
abdeldayem8 <abdeldayem8@users.noreply.github.com>
2024-11-26 14:23:51 +0000

    deploy: 0d068e752572e3e7446238e3e62e3eee03b88a5a

---
+++ assets/Newarrival-B31YoLal.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Cp9v8kIz.js";const s=()=>r.jsx("div",{children:"New Arrivals"});export{s as default};
--- assets/Newarrival-C0zn_Oq1.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CzMRR9IY.js";const s=()=>r.jsx("div",{children:"New Arrivals"});export{s as default};
