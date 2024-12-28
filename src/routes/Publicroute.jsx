6e03dcfca0e6ff2dfe5aa37ac852ef170982596a
abdeldayem8 <mhmdabdeldayem40@gmail.com>
2024-12-28 18:50:54 +0200

    .

--- src/routes/Publicroute.jsx
+++ src/routes/Publicroute.jsx
@@ -1,7 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { isAuthenticated } from "../utils/Auth"
 
-
 const Publicroute = ({children})=>{
     return isAuthenticated() ?  <Navigate to="/artivastore/profile" />  : children;
 }
